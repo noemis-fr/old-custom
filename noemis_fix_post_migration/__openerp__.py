cd9ef5dfb2f75b5b88fe7c5880f58026404bcfb2
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-11-12 13:39:45 +0100

    [FIX] post migration stock report;

---
+++ noemis_fix_post_migration/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Noemis Fix After migration',
+    'version': '1.0',
+    'category': 'Custom',
+    'author': 'Noemis',
+    'website': 'http://www.noemis.fr',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/action.xml',
+    ],
+}
